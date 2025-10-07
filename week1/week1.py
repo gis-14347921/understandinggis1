988566f9b50da73fa858780b71b9e90ee5433f65
gis-14347921 <19720564420@163.com>
2025-10-08 01:38:12 +0800

    load world shapefile as GeoDataFrame

--- week1/week1.py
+++ week1/week1.py
@@ -1,2 +1,5 @@
-bash
-conda install -c conda-forge geopandas
+from geopandas import read_file
+
+world = read_file("C:/Users/14256/Documents/GitHub/data/natural-earth/ne_50m_admin_0_countries.shp")
+
+print(world.head())
