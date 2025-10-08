c6460946a8c1ad8dbc31a515b3cc442b08c1bfba
gis-14347921 <19720564420@163.com>
2025-10-09 01:35:47 +0800

    add message here

---
+++ week2/week2.py
@@ -0,0 +1,15 @@
+# here is your list of numbers
+numbers = [1,2,3,4,5,6,7,8,9,10,15,30]
+
+# this variable will hold your result, start it at 0
+total = 0
+
+# MISSING LINE HERE
+# loop through the list
+for num in numbers:
+    # INCOMPLETE LINE HERE
+    # add each number to the total
+    total += num
+
+# print the result
+print(total)
