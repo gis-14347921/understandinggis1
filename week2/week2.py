fbb4ce3f5a1fd4a62a43c45a7e970a10547a77e4
gis-14347921 <19720564420@163.com>
2025-10-09 01:42:16 +0800

    add message here

--- week2/week2.py
+++ week2/week2.py
@@ -13,3 +13,26 @@ for num in numbers:
 
 # print the result
 print(total)
+
+from geopandas import read_file, GeoSeries
+
+# load the shapefile of countries - this gives a table of 12 columns and 246 rows (one per country)
+world = read_file("C:/Users/14256/Documents/GitHub/data/natural-earth/ne_10m_admin_0_countries.shp")
+
+# print a list of all of the columns in the shapefile
+print(world.columns)
+
+# extract the country rows as a GeoDataFrame object with 1 row
+usa = world.loc[(world.ISO_A3 == 'USA')]
+
+print(type(usa))
+
+# extract the geometry columns as a GeoSeries object
+usa_col = usa.geometry
+
+print(type(usa_col))
+
+# extract the geometry objects themselves from the GeoSeries
+usa_geom = usa_col.iloc[0]
+
+print(type(usa_geom))
